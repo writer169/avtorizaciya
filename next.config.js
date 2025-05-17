16569f1ec551bcf2e321ffd939faf0438784ebbd
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-17 18:15:02 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // Добавляем настройку для поддержки модуля async в middleware
+  experimental: {
+    serverComponentsExternalPackages: ['@clerk/nextjs']
+  }
 };
 
 module.exports = nextConfig;
