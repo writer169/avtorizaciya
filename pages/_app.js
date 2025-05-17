9c223580054488ca703693ee0e78399fb3097f97
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-17 10:19:33 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-// pages/_app.js
-import { ClerkProvider } from '@clerk/nextjs';
-import { useRouter } from 'next/router';
+import { ClerkProvider } from "@clerk/nextjs";
+import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
