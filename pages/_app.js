6fdd762749c7d2aa7451353788e1dff5a399ac06
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-17 13:19:00 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { useRouter } from "next/router";
-import "../styles/globals.css"; // If you have global styles
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
