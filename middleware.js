99e5e4426e53d4ab35597943da301cea5e52cea9
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-17 10:18:34 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,24 @@
-// middleware.js
 import { authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware();
+export default authMiddleware({
+  beforeAuth: async (req, evt) => {
+    const { user } = req.auth;
+    if (!user) return;
+
+    const { userId } = req.auth;
+    const res = await fetch(`https://api.clerk.com/v1/users/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
+      },
+    });
+    const userData = await res.json();
+
+    if (userData.public_metadata?.approved !== true) {
+      return new Response("Ожидается одобрение администратора", { status: 403 });
+    }
+  },
+});
 
 export const config = {
-  matcher: ["/protected/(.*)", "/dashboard"],
+  matcher: ["/((?!_next|favicon.ico).*)"],
 };
